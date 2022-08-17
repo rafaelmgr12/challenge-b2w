15f31c4930110d23243b0241ab4fbea75f8aec3e
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-08-17 19:31:54 -0300

    feat: add get by date

--- src/main/java/com/rafaelmgr12/challengeb2w/controller/ProductController.java
+++ src/main/java/com/rafaelmgr12/challengeb2w/controller/ProductController.java
@@ -3,11 +3,15 @@ package com.rafaelmgr12.challengeb2w.controller;
 import com.rafaelmgr12.challengeb2w.entity.Product;
 import com.rafaelmgr12.challengeb2w.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -21,5 +25,16 @@ public class ProductController {
         List<Product> products = repository.findAll();
         return ResponseEntity.ok(products);
     }
+    @GetMapping("/item")
+    public ResponseEntity<List<Product>> getProductByDate(
+            @RequestParam(value = "begindate", required = false) @DateTimeFormat(pattern="dd-MM-yyyy") Date beginDate,
+            @RequestParam(value="finaldate", required = false) @DateTimeFormat(pattern="dd-MM-yyyy") Date finalDate
+    ){
+        List<Product> products = repository.findProductByDateBetween(
+                beginDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
+                finalDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+        return ResponseEntity.ok().body(products);
+    }
+
 
 }
