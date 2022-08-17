ff056a5f997030bf43dd79d55c6505c3daf94e1e
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-08-17 17:30:46 -0300

    Start the project

---
+++ src/main/java/com/rafaelmgr12/challengeb2w/ChallengeB2wApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.challengeb2w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeB2wApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeB2wApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafaelmgr12/challengeb2w/ChallengeB2wApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.challengeb2w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeB2wApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
